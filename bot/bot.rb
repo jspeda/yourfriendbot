1d49ce2a97866579cd4ff15842b7c2fc9ff587dc
Joe Spedale <jspedale@gmail.com>
2016-10-28 21:49:15 +0000

    Added basic quote fetching

--- bot/bot.rb
+++ bot/bot.rb
@@ -10,3 +10,23 @@ class Bot < SlackRubyBot::Bot
     client.say(channel: data.channel, text: match['expression'])
   end
 end
+
+class GetaRandomQuote < SlackRubyBot::Bot
+
+random_quote = Quote.order("RANDOM()").first
+# def rand_quote
+#   rq = Quote.order("RANDOM()").first
+# end
+
+  command 'give me a quote' do |client, data, match|
+    # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying}")
+    # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying} ~ #{Quote.order("RANDOM()").first.author}")
+    client.say(channel: data.channel, text: "_#{random_quote.saying}_ ~ #{random_quote.author}")
+  end
+end
+
+class GetaSpecificQuote < SlackRubyBot::Bot
+  command 'quote #{name}' do |client, data, match|
+    client.say(channel: data.channel, text: "#{}")
+  end
+end
