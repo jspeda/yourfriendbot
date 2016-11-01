4a6e986454a2eefa4c13ecc57d3e818391335b3e
Joe Spedale <jspedale@gmail.com>
2016-11-01 23:46:41 +0000

    Added random/specific quote fetching

--- bot/bot.rb
+++ bot/bot.rb
@@ -13,20 +13,34 @@ end
 
 class GetaRandomQuote < SlackRubyBot::Bot
 
-random_quote = Quote.order("RANDOM()").first
+# random_quote = Quote.order("RANDOM()").first
 # def rand_quote
 #   rq = Quote.order("RANDOM()").first
 # end
 
-  command 'give me a quote' do |client, data, match|
-    # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying}")
-    # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying} ~ #{Quote.order("RANDOM()").first.author}")
-    client.say(channel: data.channel, text: "_#{random_quote.saying}_ ~ #{random_quote.author}")
+  # command 'give me a quote' do |client, data, match|
+  #   # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying}")
+  #   # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying} ~ #{Quote.order("RANDOM()").first.author}")
+  #   random_quote = Quote.order("RANDOM()").first
+  #   client.say(channel: data.channel, text: "_#{random_quote.saying}_ ~ #{random_quote.author}")
+  # end
+end
+
+# class GetaSpecificQuote < SlackRubyBot::Bot
+#   command 'quote #{name}' do |client, data, match|
+#     client.say(channel: data.channel, text: "")
+#   end
+# end
+
+class GetSpecificQuote < SlackRubyBot::Bot
+  match /^Give me a quote from (?<author>\w*)$/ do |client, data, match|
+    quote = Quote.where(author: match[:author]).order("RANDOM()").first
+    client.say(channel: data.channel, text: "_#{quote.saying}_ ~ #{quote.author}")
   end
 end
 
-class GetaSpecificQuote < SlackRubyBot::Bot
-  command 'quote #{name}' do |client, data, match|
-    client.say(channel: data.channel, text: "#{}")
+class Weather < SlackRubyBot::Bot
+  match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+    client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
   end
 end
