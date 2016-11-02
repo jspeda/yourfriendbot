9e2efd0b3dd62f63572b6c00446d5614701bd63c
Joe Spedale <jspedale@gmail.com>
2016-11-02 00:26:05 +0000

    cleaned up bot.rb file

--- bot/bot.rb
+++ bot/bot.rb
@@ -12,35 +12,21 @@ class Bot < SlackRubyBot::Bot
 end
 
 class GetaRandomQuote < SlackRubyBot::Bot
-
-# random_quote = Quote.order("RANDOM()").first
-# def rand_quote
-#   rq = Quote.order("RANDOM()").first
-# end
-
-  # command 'give me a quote' do |client, data, match|
-  #   # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying}")
-  #   # client.say(channel: data.channel, text: "#{Quote.order("RANDOM()").first.saying} ~ #{Quote.order("RANDOM()").first.author}")
-  #   random_quote = Quote.order("RANDOM()").first
-  #   client.say(channel: data.channel, text: "_#{random_quote.saying}_ ~ #{random_quote.author}")
-  # end
+   command 'random quote' do |client, data, match|
+     random_quote = Quote.order("RANDOM()").first
+     client.say(channel: data.channel, text: "_#{random_quote.saying}_ ~ #{random_quote.author}")
+   end
 end
 
-# class GetaSpecificQuote < SlackRubyBot::Bot
-#   command 'quote #{name}' do |client, data, match|
-#     client.say(channel: data.channel, text: "")
-#   end
-# end
-
 class GetSpecificQuote < SlackRubyBot::Bot
-  match /^Give me a quote from (?<author>\w*)$/ do |client, data, match|
+  match /^quote (?<author>\w*)$/ do |client, data, match|
     quote = Quote.where(author: match[:author]).order("RANDOM()").first
     client.say(channel: data.channel, text: "_#{quote.saying}_ ~ #{quote.author}")
   end
 end
 
-class Weather < SlackRubyBot::Bot
-  match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
-    client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
-  end
-end
+# class Weather < SlackRubyBot::Bot
+#   match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+#     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+#   end
+# end
