d15d6898a48b72fb5f4ac9ba565cd0e6187de2de
Joe Spedale <jspedale@gmail.com>
2016-11-13 00:02:28 +0000

    Added Spotify search functions and help page

--- bot/bot.rb
+++ bot/bot.rb
@@ -35,28 +35,67 @@ end
 #     end}")
 #   end
 
-class SpotifyAlbumList < SlackRubyBot::Bot
-  match /^albums (?<artist>\w+)$/ do |client, data, match|
+class SpotifySearch < SlackRubyBot::Bot
+  # finds an artist's albums
+  match /^albums (?<artist>.+)$/ do |client, data, match|
     artist_link = RSpotify::Artist.search(match[:artist]).first
     client.say(channel: data.channel, text: "#{artist_link.albums.map { |x| x.name }.uniq}")
   end
-end
-
-
-  # def list_albums(artist)
-  #   albums = artist.albums
-  #   albums.map { |x| x.name }
-  # end
-
-class SpotifySongSearch < SlackRubyBot::Bot
-  match /^song (?<artist>\w+) - (?<title>\w+)$/ do |client, data, match|
+  # finds an artist's top tracks
+  match /^top_tracks (?<artist>.+)$/ do |client, data, match|
     artist_link = RSpotify::Artist.search(match[:artist]).first
-    client.say(channel: data.channel, text: "#{artist_link.albums.uniq.map { |x| x.name}}")
+    client.say(channel: data.channel, text: "#{artist_link.top_tracks(:US).map { |x| x.name }.uniq}")
   end
+  # links a random song from specified artist's top tracks
+  match /^random_song (?<artist>.+)$/ do |client, data, match|
+    artist_link = RSpotify::Artist.search(match[:artist]).first
+    client.say(channel: data.channel, text: "#{artist_link.top_tracks(:US).sample.external_urls}")
+  end
+
+  # links to first result of song search
+  match /^play (?<title>.+)$/ do |client, data, match|
+    song_link = RSpotify::Track.search(match[:title]).first
+    client.say(channel: data.channel, text: "#{song_link.external_urls}")
+  end
+
+  help do
+    title 'Your Friend'
+    desc 'This bot is your friend and will always be there for you'
+
+    command 'random quote' do
+      desc 'Gives you a random quote from the esteemed members of this community'
+    end
+
+    command 'quote <Lastname>' do
+      desc "Gives you a quote from a specific person. Correct usage(excluding brackets): [quote Obama]\n" \
+      'Incorrect usage: [quote Barack]'
+    end
+
+    command 'albums <artist>' do
+      desc 'lists the albums (and singles) for the specified artist via Spotify'
+    end
+
+    command 'top_tracks <artist>' do
+      desc 'lists the top tracks for the specified artist via Spotify'
+    end
+
+    command 'random_song <artist>' do
+      desc 'links a random song from the specified artist\'s top tracks via Spotify'
+    end
+
+    command 'play <song title>' do
+      desc 'links the first result of the song title searched via Spotify'
+    end
+
+
+  end
+
 end
 
-# class Weather < SlackRubyBot::Bot
-#   match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
-#     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+
+# class SpotifySongSearch < SlackRubyBot::Bot
+#   match /^song (?<artist>.*) - (?<title>.*)$/ do |client, data, match|
+#     artist_link = RSpotify::Artist.search(match[:artist]).first
+#     client.say(channel: data.channel, text: "#{artist_link.albums.uniq.map { |x| x.name}}")
 #   end
 # end
